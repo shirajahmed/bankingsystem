75fa21e2935d22a770a639d1048c015f96c9c82e
shirajahmed <sairajad690@hail.com>
2024-06-09 02:17:33 +0530

    right side bar fo deshboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,39 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = {firstName: 'Shiraj'};
+  const loggedIn = {
+    firstName: "Shiraj",
+    lastName: "Ahmed",
+    email: "shiraj@gmail.com",
+  };
   return (
-    <section className='home'>
+    <section className="home">
       <div className="home-content">
-        <header className='home-header'>
-         <HeaderBox 
-         type="greeting"
-         title="Welcome"
-         subtext="Access and mange account "
-         user={loggedIn?.firstName || 'Guest'}
-         />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Access and mange account "
+            user={loggedIn?.firstName || "Guest"}
+          />
 
-         <TotalBalanceBox 
-         accounts={[]}
-         totalBanks={1}
-         totalCurrentBalance={1234.00}
-         />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1234.0}
+          />
         </header>
+        RECENT TRANSCTION
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 12.0 }, { currentBalance: 12.0 }]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
