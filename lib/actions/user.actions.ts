a40492046ea72a47c4bf1fd924890616cd2a48db
shirajahmed <sairajad690@hail.com>
2024-06-16 18:40:58 +0530

    signin singn up  next apprite and backend

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // mutation database action
+  } catch (error) {
+    console.log("Error useraction signIN", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // mutation database action
+  } catch (error) {
+    console.log("Error useraction signIN", error);
+  }
+};
